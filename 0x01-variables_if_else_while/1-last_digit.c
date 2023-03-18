7cd30f9d41958eccff9605a0d6ce63ebd5cdb65b
Eric Ishimwe <ishimwericio@gmail.com>
2023-03-17 17:15:48 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-last_digit.c -o 1-last_digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print the last digit of a number stored in n
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+	int m,
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	m = n % 10;
+	if (m > 5)
+		printf("Last digit of %d is %d and is grater than 5", n, m);
+	else if (m == 0)
+		printf("Last digit of %d is %d and is 0", n, m);
+	else (m < 6 && m != 0)
+		printf("Last digit of %d id %d and is less than 6 and not 0", n, m);
+	printf("\n");
+	return (0);
+}
